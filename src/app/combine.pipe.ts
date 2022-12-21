30b15101591514ed57222f3716c57546328227f8
sbehera <sbehera@expediagroup.com>
2022-12-22 00:55:41 +0530

    added optional params to custom pipe

--- src/app/combine.pipe.ts
+++ src/app/combine.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({ name: 'combine' })
 export class CombinePipe implements PipeTransform {
-    transform(value: any[], ...args: any[]): any {
-        return value.join('$ ');
+    transform(value: any[], separator = ', '): any {
+        return value.join(separator);
     }
 }
